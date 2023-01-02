87e851252ff779be445fce5e12982b22c6cbae1a
yuntaeyang <swj01234@naver.com>
2023-01-02 12:07:21 +0900

    단어정렬 commit

---
+++ 단어정렬.py
@@ -0,0 +1,16 @@
+def main():
+    n = int(input())
+    lst = []
+
+    for i in range(n):
+        lst.append(input())
+    set_lst = set(lst)	
+    lst = list(set_lst)	
+    lst.sort()
+    lst.sort(key = len)
+
+    for i in lst:
+        print(i)
+
+if __name__ == "__main__":
+    main()
